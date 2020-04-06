21910fabd64634926723d68da9d92bd33079d296
MarkerToSummer <krasusworks@gmail.com>
2020-04-06 23:37:22 +0700

    City register db + cheching mechanism + first test (person in? not temporary)

--- city-register/src/main/java/mts/city/dao/PersonCheckDao.java
+++ city-register/src/main/java/mts/city/dao/PersonCheckDao.java
@@ -12,32 +12,52 @@ public class PersonCheckDao {
             "inner join cr_person p on p.person_id = ap.person_id " +
             "inner join cr_adress a on a.adress_id = ap.adress_id " +
             "where " +
-            "upper(p.sur_name COLLATE \"en_US.UTF8\") = upper(? COLLATE \"en_US.UTF8\") " +
-            "and upper(p.given_name COLLATE  \"en_US.UTF8\") = upper(? COLLATE  \"en_US.UTF8\") " +
-            "and upper(patronymic COLLATE  \"en_US.UTF8\") = upper(? COLLATE  \"en_US.UTF8\") " +
+            "CURRENT_DATE >= ap.start_date and (CURRENT_DATE <= ap.END_date or ap.END_date is null)" +
+            "and p.sur_name = ? " +
+            "and p.given_name  = ? " +
+            "and p.patronymic = ? " +
             "and p.date_of_birth = ? " +
             "and a.street_code = ? " +
-            "and upper(a.building COLLATE  \"en_US.UTF8\") = upper(? COLLATE  \"en_US.UTF8\") " +
-            "and upper(extension COLLATE  \"en_US.UTF8\") = upper(? COLLATE  \"en_US.UTF8\") " +
-            "and upper(a.apartment COLLATE  \"en_US.UTF8\") = upper(? COLLATE  \"en_US.UTF8\")";
+            "and a.building = ? ";
 
     public PersonResponse checkPerson (PersonRequest request) throws PersonCheckException {
+
         PersonResponse response = new PersonResponse();
 
+        String sql = SQL_REQUEST;
+
+        if (request.getExtention() != null) {
+            sql += " and a.extension = ? ";
+        } else {
+            sql += " and a.extension is null ";
+        }
+
+        if (request.getApartment() != null) {
+            sql += " and a.apartment = ? ";
+        } else {
+            sql += " and a.apartment is null ";
+        }
 
         try (Connection connection = getConnection();
-             PreparedStatement ps = connection.prepareStatement(SQL_REQUEST)
+             PreparedStatement ps = connection.prepareStatement(sql)
         ) {
-            ps.setString(1, request.getSurName());
-            ps.setString(2, request.getGivenName());
-            ps.setString(3, request.getPatronymicName());
-            ps.setDate(4, java.sql.Date.valueOf(request.getDateOfBirth()));
-            ps.setInt(5, request.getStreetCode());
-            ps.setString(6, request.getBuilding());
-            ps.setString(7, request.getExtention());
-            ps.setString(8, request.getApartment());
+            int count = 1;
+            ps.setString(count++, request.getSurName());
+            ps.setString(count++, request.getGivenName());
+            ps.setString(count++, request.getPatronymicName());
+            ps.setDate(count++, java.sql.Date.valueOf(request.getDateOfBirth()));
+            ps.setInt(count++, request.getStreetCode());
+            ps.setString(count++, request.getBuilding());
+
+            if (request.getExtention() != null) {
+                ps.setString(count++, request.getExtention());
+            }
+            if (request.getApartment() != null) {
+                ps.setString(count, request.getApartment());
+            }
 
             ResultSet rs = ps.executeQuery();
+
             if (rs.next()) {
                 response.setRegistered(true);
                 response.setTemporal(rs.getBoolean("temporal"));
@@ -56,3 +76,17 @@ public class PersonCheckDao {
                 "root");
     }
 }
+
+/*
+private static final String SQL_REQUEST = "select temporal from cr_adress_person ap " +
+            "inner join cr_person p on p.person_id = ap.person_id " +
+            "inner join cr_adress a on a.adress_id = ap.adress_id " +
+            "where " +
+            "CURRENT_DATE >= ap.start_date and (CURRENT_DATE <= ap.END_date or ap.END_date is null)" +
+            "and upper(p.sur_name COLLATE \"ru_RU.UTF-8\") = upper(? COLLATE \"ru_RU.UTF-8\") " +
+            "and upper(p.given_name COLLATE  \"ru_RU.UTF-8\") = upper(? COLLATE  \"ru_RU.UTF-8\") " +
+            "and upper(patronymic COLLATE  \"ru_RU.UTF-8\") = upper(? COLLATE  \"ru_RU.UTF-8\") " +
+            "and p.date_of_birth = ? " +
+            "and a.street_code = ? " +
+            "and upper(a.building COLLATE  \"ru_RU.UTF-8\") = upper(? COLLATE  \"ru_RU.UTF-8\") ";
+ */
