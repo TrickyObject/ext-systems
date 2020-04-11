62043fca99a815f3e87a7387f96d2247279c202e
MarkerToSummer <krasusworks@gmail.com>
2020-04-11 22:57:47 +0700

    Added the dependency that provide servlet technology. Create GET-request via annotation.

---
+++ city-register/src/main/java/mts/city/web/CheckPersonServlet.java
@@ -0,0 +1,21 @@
+package mts.city.web;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CheckPersonServlet", urlPatterns = {"/checkPerson"})
+public class CheckPersonServlet extends HttpServlet {
+
+    // 2 варианта передать инфо о классе в Tomcat:
+    // web.xml (старый) и аннотация
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.getWriter().println("GET-method CheckPerson called");
+    }
+}
