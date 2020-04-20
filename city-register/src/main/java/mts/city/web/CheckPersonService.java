bcf42d35e5cf158539adb8d4ebbaef84cc886ff2
MarkerToSummer <krasusworks@gmail.com>
2020-04-21 01:45:06 +0700

    REST-реализация

---
+++ city-register/src/main/java/mts/city/web/CheckPersonService.java
@@ -0,0 +1,15 @@
+package mts.city.web;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/check")
+public class CheckPersonService {
+
+    @GET
+    public String checkPerson() {
+
+        return "Simple sting";
+    }
+
+}
